58e5309596b7bac6af45a3e46d81ac0e175d32ec
Steve DeRose <sderose@classifyprocess.org>
2020-03-17 12:27:13 -0400

    Add --findSet to ord. Updte spaces.py.

--- Unicode/asPython/spaces.py
+++ Unicode/asPython/spaces.py
@@ -5,9 +5,10 @@ USpaces = {
     0x0000C:  "FORM FEED",
     0x0000D:  "CARRIAGE RETURN",
     0x00020:  "SPACE",
-    0x00089:  "CHARACTER TABULATION WITH JUSTIFICATION",
-    0x000A0:  "NO-BREAK SPACE",
+    0x00089:  "CHARACTER TABULATION WITH JUSTIFICATION",  # Not in Perl \s
+    0x000A0:  "NO-BREAK SPACE",                  # Not in Perl \s
     0x01680:  "OGHAM SPACE MARK",
+    0x0180E:  "MONGOLIAN VOWEL SEPARATOR",       # In Perl \s
     0x02000:  "EN QUAD",
     0x02001:  "EM QUAD",
     0x02002:  "EN SPACE",
@@ -19,10 +20,12 @@ USpaces = {
     0x02008:  "PUNCTUATION SPACE",
     0x02009:  "THIN SPACE",
     0x0200A:  "HAIR SPACE",
-    0x0200B:  "ZERO WIDTH SPACE",
+    0x02028:  "LINE SEPARATOR",                  # In Perl \s
+    0x02029:  "PARAGRAPH SEPARATOR",             # In Perl \s
+    0x0200B:  "ZERO WIDTH SPACE",                # Not in Perl \s
     0x0202F:  "NARROW NO-BREAK SPACE",
     0x0205F:  "MEDIUM MATHEMATICAL SPACE",
-    0x02420:  "SYMBOL FOR SPACE",
+    0x02420:  "SYMBOL FOR SPACE",                # Not in Perl \s
     0x03000:  "IDEOGRAPHIC SPACE",
-    0x0303F:  "IDEOGRAPHIC HALF FILL SPACE",
+    0x0303F:  "IDEOGRAPHIC HALF FILL SPACE",     # Not in Perl \s
 }
