63aa1441dfdb3bb64ddebbf8d7b827ca7abda3fd
Steven J. DeRose <sderose@docugami.com>
2021-09-01 16:02:37 -0400

    Add shaders char list.

---
+++ UnicodeLists/shaders.py
@@ -0,0 +1,33 @@
+# Unicode characters for degrees of shading
+#
+shadeBlocks = {
+    chr(0x02580): 'UPPER HALF BLOCK',
+
+    chr(0x02581): 'LOWER ONE EIGHTH BLOCK',
+    chr(0x02582): 'LOWER ONE QUARTER BLOCK',
+    chr(0x02583): 'LOWER THREE EIGHTHS BLOCK',
+    chr(0x02584): 'LOWER HALF BLOCK',
+    chr(0x02585): 'LOWER FIVE EIGHTHS BLOCK',
+    chr(0x02586): 'LOWER THREE QUARTERS BLOCK',
+    chr(0x02587): 'LOWER SEVEN EIGHTHS BLOCK',
+    chr(0x02588): 'FULL BLOCK',
+
+    chr(0x02589): 'LEFT SEVEN EIGHTHS BLOCK',
+    chr(0x0258a): 'LEFT THREE QUARTERS BLOCK',
+    chr(0x0258b): 'LEFT FIVE EIGHTHS BLOCK',
+    chr(0x0258c): 'LEFT HALF BLOCK',
+    chr(0x0258d): 'LEFT THREE EIGHTHS BLOCK',
+    chr(0x0258e): 'LEFT ONE QUARTER BLOCK',
+    chr(0x0258f): 'LEFT ONE EIGHTH BLOCK',
+
+    chr(0x02590): 'RIGHT HALF BLOCK',
+
+    chr(0x02594): 'UPPER ONE EIGHTH BLOCK',
+    chr(0x02595): 'RIGHT ONE EIGHTH BLOCK',
+}
+
+shades = {
+    chr(0x02591): 'LIGHT SHADE',
+    chr(0x02592): 'MEDIUM SHADE',
+    chr(0x02593): 'DARK SHADE',
+}
