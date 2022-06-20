3b79c4b81aff1a6c744a64d7092ac35f521db1e9
Steven J. DeRose <>
2022-06-20 17:55:20 -0400

    charNameConvert: get --chart working. from/to conversions next.

--- charNameConvert.py
+++ charNameConvert.py
@@ -26,7 +26,7 @@ def info1(msg:str) -> None:
     if (args.verbose >= 1): lg.info(msg)
 def info2(msg:str) -> None:
     if (args.verbose >= 2): lg.info(msg)
-def fatal(msg:str) -> None: 
+def fatal(msg:str) -> None:
     lg.critical(msg); sys.exit()
 
 __metadata__ = {
@@ -46,18 +46,18 @@ __version__ = __metadata__["modified"]
 descr = """
 =Name=
     """ +__metadata__["title"] + ": " + __metadata__["description"] + """
-    
-    
+
+
 =Description=
 
-Provide Python interface to a set of "extended" information about special character
+Provide a Python interface to a set of "extended" information about special character
 names provided by many organizations. These
 are based on (imho, superb) work by my old friend Sebastian Rahtz,
 David Carlisle, and others [https://www.w3.org/Math/characters/unicode.xml].
 
 ==Usage from code==
 
-This package (currently) only handles conversion of code points and bare names.
+The API (currently) only handles conversion of code points and bare names.
 It doesn't know anything about HTML "&", TEX "\\", etc. So if you want to recognize
 or transform references in a particular document/file syntax, you'll need to do
 that on top. One possible example, to go from TEX to HTML special characters,
@@ -66,11 +66,11 @@ such as \\phi to &phgr;:
     from charNameConvert import charNameConvert
     cnc = charNameConvert(os.environ["HOME"]+"/myStuff/unicode.xml")
     cmap:Dict = cnc.getMap(CharStd.latex, CharStd.html)
-    
+
     def fixChar(m):
         if "\\"+m.group(1) in cmap: return "&" + cmap[m.group(1) + ";"]
         return m
-    
+
     for rec in sys.stdin.readlines():
         rec = re.sub(r"\\\\(\\w+)(?![\\[\\{])", fixChar, rec)
         print rec
@@ -78,20 +78,55 @@ such as \\phi to &phgr;:
 Note that unlike most other data in the source, latex, varlatex, IEEE, AMS, and Springer
 include backslashes. In some cases, latex has to also include more complex
 markup, such as achieving Unicode MATHEMATICAL BOLD via \\mathbf{X}
+
 ==Usage on the command line==
 
-    charNameConvert.py --to [name] [options] [files]
+    charNameConvert.py --from [name1] --to [name2] [options] [files]
 
-This will convert stdin by replacing any characters outside the normal ASCII range,
-to their equivalent in the --to standard. You can set a --prefix and --suffix to put
-around them if desired, either for syntactic or visual purposes:
+This will convert stdin by replacing any character references in the input,
+which are encoded by the syntax rules of system [name1],
+to their equivalent in the --to standard. For example:
 
-    charNameConvert.py --to html --prefix "&" --suffix ";"
+    charNameConvert.py --from LaTeX --to html
+
+would change \\phi to &phgr;, among many others.
+
+You can omit --from, in which case literal characters outside the normal
+ASCII range will be affected. The input character set defaults to utf-8,
+but can be changed with --iencoding.
+
+Similarly, if you omit --to, characters whose encoding is recognized in the
+input, will be written out as literals in the output.
+
+I expect to add options to handle numeric character references as well.
+
+The input and output formats supported are (for U+2022 BULLET as example):
+
+* latex    -- \\textbullet
+* xml      -- &bull;
+* xml10    -- &#8226;
+* xml16    -- &#x2022;
+* literal  -- [an encoded character per se]
+
+The "xml" cases are slightly special, because the source data only includes
+the name, not the "&" and ";". So these are recognized and generated
+specially. Also,
+you can specify --fallback for what to do if a character is recognized, but
+does not exist in the requested output format. It can be one of:
+
+* "unchanged"
+* "literal"
+* "xml10"
+* "xml16" (this is the default)
+
+For example, a character with no html named entity, will be written as &#x___;,
+where "___" is the hex character code, if you specify --fallback xml10.
+
 
 ==Names of supported character standards==
 
-The source-names used here are mostly the same as in the source data (sample below). 
-But note that different SGML or XML 
+The source-names used here are mostly the same as in the source data (sample below).
+But note that different SGML or XML
 schemas can define their own entity names, which may use the same name in
 different ways. Therefore, we use the names of the entity sets (such as "html4-symbol")
 rather than just "entity":
@@ -106,14 +141,14 @@ rather than just "entity":
     * isopub
     * mmlalias
     * description (this is generally the full unicode name)
-    
+
 Particular font position is also available, via two properties:
     * fontname
     * fontpos="1"
 
 Finally, for the "to" side of a conversion, you can specify these forms:
     * slashu -- the hex Unicode code point, like \uFFFF
-    
+
 =Source format=
 
 After some preliminaries, the data consists of a long series of elements like this:
@@ -138,16 +173,18 @@ After some preliminaries, the data consists of a long series of elements like th
  <description>BULLET</description>
 </character>
 
+The data sometimes has entries like this, which are presently ignored:
+    <surrogate mathvariant="sans-serif-bold-italic" ref="U003D6"/>
 
 =Related commands and data=
 
 The source data on character names is from [https://www.w3.org/Math/characters/unicode.xml].
 A copy is also available at [https://github.com/sderose/Charsets.git/blob/master/unicode.xml].
 
-    
+
 =Known bugs and Limitations=
 
-Some entries are for a combination of characters, as shown below. These generate 
+Some entries are for a combination of characters, as shown below. These generate
 a warning during loading (except with -q), and are discarded.
 
     <character id="U0003C-020D2" dec="60-8402" type="other" mode="unknown">
@@ -225,37 +262,48 @@ class CharStdInfo:
     def __init__(self, codePoint:int):
         assert codePoint >= 0 and codePoint <= 0x1FFFF
         self.codePoint  = codePoint
-        self.names = [ None for i in range(CHARSTD_MIN, CHARSTD_MAX+1) ]
+        self.names = [ None for i in range(CHARSTD_MAX+1) ]
 
     def addStd(self, whichStd:CharStd, value:str):
         stdNum = int(whichStd)
         try:
-            if (self.names[stdNum] is not None): 
+            if (self.names[stdNum] is not None):
                 print("Duplicate prop %s for %05x." % (whichStd.name, self.codePoint))
                 return False
             self.names[stdNum] = value
         except IndexError as e:
-            print("Can't set prop %s for code point %05x.\n    %s" %
-                (whichStd.name, self.codePoint, e))
+            print("Can't set prop '%s' (stdNum %d) for code point %05x.\n    %s" %
+                (whichStd.name, stdNum, self.codePoint, e))
             return False
         return True
-                
+
     def tostring(self, exclude:Dict) -> str:
-        buf = "U+%05x: \n"
+        buf = "U+%05x: \n" % self.codePoint
         for i in range(CHARSTD_MIN, CHARSTD_MAX+1):
             stdName = CharStd(i).name
             if (exclude and stdName in exclude): continue
             try:
-                curName = self.names[i]
+                if (CharStd(i) == CharStd.html4): curName = self.getXML(self.codePoint)
+                else: curName = self.names[i]
             except IndexError as e:
                 print("std #%d (%s) out of range:\n    %s" % (i, stdName, e))
             if (curName is None): curName = "--"
             buf += "    %-12s: %s\n" % (stdName, curName)
         return buf
-        
-    
+
+    def getXML(self, codePoint:int):
+        x = self.names[CharStd.html4]
+        print("getXML for cp %05x, html name is: %s" % (codePoint, x if x else "[none]"))
+        if (x is not None): return "&%s;" % (x)
+        if (args.fallback == "unchanged"): return "unchanged"
+        if (args.fallback == "xml10"):   return "&#%04d;" % (codePoint)
+        if (args.fallback == "xml16"):   return "&#%04x;" % (codePoint)
+        if (args.fallback == "literal"): return chr(codePoint)  # not checking gt lt etc.
+        assert False, "Bad --fallback value '%s'." % (args.fallback)
+
+
 ###############################################################################
-# See charNameConvert.py for 
+# See charNameConvert.py for
 #
 propNames = """{
     "ACS":          ( F, X,    S,  str, "freq:61", ),
@@ -301,7 +349,7 @@ class charNameConvert(dict):
         self.sourceUrl = "https://www.w3.org/Math/characters/unicode.xml"
         self.charDict = {}
         self.nCombinations = 0
-        
+
         if (path is None):
             self.path = os.path.join(os.environ["HOME"], ".strfchr", "unicode.xml")
         else:
@@ -313,11 +361,11 @@ class charNameConvert(dict):
             check_output([ "curl", self.sourceUrl, ">>", self.path ])
         if (not os.path.exists(self.path)):
             lg.fatal("Could not download data from '%s'.", self.sourceUrl)
-            
+
     def loadData(self):
         # Pick which ones to show with tostring()
         excl = set([
-            #"description", 	"html4", 	"latex", 	    
+            #"description", 	"html4", 	"latex",
             "mathlatex", 	"varlatex",
             "ACS", 	        "AIP", 	        "APS", 	    "IEEE", 	"Springer",
             "Wolfram", 	    "isopub", 	    "mmlalias", "surrogate",
@@ -325,7 +373,7 @@ class charNameConvert(dict):
         ])
 
         self.downloadData()
-        
+
         #DomExtensions.DomExtensions.patchDom()
         xdom = xml.dom.minidom.parse(self.path)
         #print(xdom.toprettyxml())
@@ -333,14 +381,14 @@ class charNameConvert(dict):
         charList = xdom.documentElement
         assert charList.nodeName == "charlist"
         print("charList child count: %d" % (len(charList.childNodes)))
-        
+
         nChars = 0
         self.charDict = {}
         for charEl in charList.childNodes:
             if (charEl.nodeName != "character"): continue
             idVal = charEl.getAttribute("id")
             dec = charEl.getAttribute("dec")
-            print("loading char %5s (d%06s)" % (idVal, dec))
+            if (args.verbose): print("loading char %5s (d%06s)" % (idVal, dec))
             if ("-" in idVal):
                 if not args.quiet:
                     lg.warning("Combination character, id '%s' (ignored).", idVal)
@@ -353,9 +401,9 @@ class charNameConvert(dict):
                 assert int(idVal[1:],16) == dec
             except ValueError as e:
                 print("ValueError (idVal '%s', dec '%s') in:\n%s\n%s" %
-                    (idVal, dec, charEl.toprettyxml(), e))
+                    (idVal, dec, charEl.toprettyxml() if args.verbose else "", e))
                 continue
-                
+
             ci = CharStdInfo(dec)
             self.charDict[dec] = ci
             nChars += 1
@@ -375,6 +423,7 @@ class charNameConvert(dict):
                 elif (prop == "Springer"):  rc = ci.addStd(CharStd.Springer, val)
                 elif (prop == "APS"):       rc = ci.addStd(CharStd.APS, val)
                 elif (prop == "Wolfram"):   rc = ci.addStd(CharStd.Wolfram, val)
+                elif (prop == "surrogate"): continue
                 elif (prop == "entity"):
                     prop = propEl.getAttribute("set")
                     if (prop == "html-symbol"): prop = "html"
@@ -388,7 +437,7 @@ class charNameConvert(dict):
                         assert int(pos) >= 0 and int(pos) < 0x1FFFF
                     except (AssertionError, ValueError):
                         print("font: @pos '%s' bad for name '%s':\n%s" %
-                            (pos, nam, propEl.toprettyxml()))
+                            (pos, nam, propEl.toprettyxml() if args.verbose else ""))
                     val = nam + " " + pos
                     rc = ci.addStd(CharStd.font, val)
                 elif (prop == "description"):
@@ -398,7 +447,7 @@ class charNameConvert(dict):
                         lg.warning("Unexpected spec '%s'.", prop)
                 if (not rc):
                     lg.warning("******* Problem with prop '%s', val '%s' in:\n%s",
-                        prop, val, charEl.toprettyxml())
+                        prop, val, charEl.toprettyxml() if args.verbose else "")
             print(ci.tostring(exclude=excl))
         lg.info("Char defs processed: %d.", nChars)
         assert len(self.charDict) == nChars
@@ -424,23 +473,23 @@ class charNameConvert(dict):
         for ch in node.childNodes:
             if (ch.nodeName == ename): return ch
         return None
-        
-        
+
+
 ###############################################################################
 #
 def doChart():
-    print("Starting chart.")
+    print("Starting chart, LaTeX to XML.")
     cnc = charNameConvert(os.environ["sjdUtilsDir"]+"/Public/CharSets/unicode.xml")
     print("%d chars loaded." % (len(cnc.charDict)))
     cnmap:Dict = cnc.getMap(CharStd.latex, CharStd.html4)
-    
+
     # Collect all the LaTeX special-char strings
     latexCodes = {}
     for codePoint, info in cnmap.items():
         if (info.names[CharStd.latex] is None): continue
         latexCodes[info.names[CharStd.latex]] = (codePoint, info.names[CharStd.html4])
-    lg.info("latex special characters found: %d.", len(latexCodes))
-    
+    print("latex special characters found: %d.", len(latexCodes))
+
     opener = """
 \\documentclass{article}
 \\usepackage[english]{babel}
@@ -453,11 +502,11 @@ def doChart():
 \\author{charNameConvert.py}
 """
     print(opener)
-    
+
     for latexCode in sorted(latexCodes.keys()):
         cp, ent = latexCodes[latexCodes]
         print("    %s & ^^^^%04x & \\&%s; //" % (latexCode, cp, ent))
-    
+
     print("""
     -30-
 """)
@@ -480,7 +529,7 @@ def doOneFile(path:str) -> int:
 
     cnc = charNameConvert(os.environ["sjdUtilsDir"]+"/Public/CharSets/unicode.xml")
     cmap = cnc.getMap(CharStd.latex, CharStd.html4)
-    
+
     for rec in fh.readlines():
         rec = re.sub(r"\\\\(\\w+)(?![\\[\\{])", fixChar, rec)
         print(rec)
@@ -488,7 +537,7 @@ def doOneFile(path:str) -> int:
 def fixChar(m):
     if "\\"+m.group(1) in cmap: return "&" + cmap[m.group(1) + ";"]
     return m
-    
+
 
 ###############################################################################
 # Main
@@ -511,11 +560,23 @@ if __name__ == "__main__":
             "--chart", action="store_true",
             help="Output a chart in TEX, of all the TEX characters.")
         parser.add_argument(
+            "--fallback", type=str, default="xml16",
+            choices=[ "literal", "xml10", "xml16", "unchanged" ],
+            help="With --to xml, if no xml named entity if available, use this form.")
+        parser.add_argument(
+            "--from", type=str, default="literal",
+            choices=[ "literal", "xml", "xml10", "xml16", "latex" ],
+            help="Input in this format.")
+        parser.add_argument(
             "--oencoding", type=str, metavar="E", default="utf-8",
             help="Use this character coding for output. Default: iencoding.")
         parser.add_argument(
             "--quiet", "-q", action="store_true",
             help="Suppress most messages.")
+        parser.add_argument(
+            "--to", type=str, default="literal",
+            choices=[ "literal", "xml", "xml10", "xml16", "latex" ],
+            help="Output in this format.")
         parser.add_argument(
             "--unicode", action="store_const", dest="iencoding",
             const="utf8", help="Assume utf-8 for input files.")
@@ -549,7 +610,7 @@ if __name__ == "__main__":
     if (args.chart):
         doChart()
         sys.exit()
-        
+
     if (len(args.files) == 0):
         info0("charNameConvert.py: No files specified....")
         doOneFile(None)
